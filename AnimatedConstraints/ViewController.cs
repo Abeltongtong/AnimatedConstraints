b7188e1e6c975d547f190f224d7db46478b98b04
Abel Hu <abel@ruthenium.local>
2016-04-07 14:09:52 +0800

    commit

--- AnimatedConstraints/ViewController.cs
+++ AnimatedConstraints/ViewController.cs
@@ -22,44 +22,42 @@ namespace AnimatedConstraints
 			bool isBlue = true;
 			bool isRed = true;
 			// Perform any additional setup after loading the view, typically from a nib.
-			View.BackgroundColor = UIColor.White;
 
-			UIButton redView = new UIButton ();
-			redView.BackgroundColor = UIColor.Red;
-			redView.TranslatesAutoresizingMaskIntoConstraints = false;
+			UIButton redButton = new UIButton ();
+			redButton.BackgroundColor = UIColor.Red;
+			redButton.TranslatesAutoresizingMaskIntoConstraints = false;
 
-			Add (redView);
+			Add (redButton);
 
-			UIButton blueView = new UIButton ();
-			blueView.BackgroundColor = UIColor.Green;
-			blueView.TranslatesAutoresizingMaskIntoConstraints = false;
-			Add (blueView);
+			UIButton greenButton = new UIButton ();
+			greenButton.BackgroundColor = UIColor.Green;
+			greenButton.TranslatesAutoresizingMaskIntoConstraints = false;
+			Add (greenButton);
 
-			View.AddConstraint (NSLayoutConstraint.Create (redView, NSLayoutAttribute.Width, NSLayoutRelation.Equal, blueView, NSLayoutAttribute.Width, 1, 0));
-			View.AddConstraint (NSLayoutConstraint.Create (redView, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, View, NSLayoutAttribute.Bottom, 1, -10));
-			View.AddConstraint (NSLayoutConstraint.Create (redView, NSLayoutAttribute.Top, NSLayoutRelation.Equal, View, NSLayoutAttribute.Top, 1, 10));
-			View.AddConstraint (NSLayoutConstraint.Create (redView, NSLayoutAttribute.Right, NSLayoutRelation.Equal, blueView, NSLayoutAttribute.Left, 1, -10));
+			View.AddConstraint (NSLayoutConstraint.Create (redButton, NSLayoutAttribute.Width, NSLayoutRelation.Equal, greenButton, NSLayoutAttribute.Width, 1, 0));
+			View.AddConstraint (NSLayoutConstraint.Create (redButton, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, View, NSLayoutAttribute.Bottom, 1, -10));
+			View.AddConstraint (NSLayoutConstraint.Create (redButton, NSLayoutAttribute.Top, NSLayoutRelation.Equal, View, NSLayoutAttribute.Top, 1, 10));
+			View.AddConstraint (NSLayoutConstraint.Create (redButton, NSLayoutAttribute.Right, NSLayoutRelation.Equal, greenButton, NSLayoutAttribute.Left, 1, -10));
+			View.AddConstraint (NSLayoutConstraint.Create (greenButton, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, View, NSLayoutAttribute.Bottom, 1, -10));
+			View.AddConstraint (NSLayoutConstraint.Create (greenButton, NSLayoutAttribute.Top, NSLayoutRelation.Equal, View, NSLayoutAttribute.Top, 1, 10));
 
-
-			View.AddConstraint (NSLayoutConstraint.Create (blueView, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, View, NSLayoutAttribute.Bottom, 1, -10));
-			View.AddConstraint (NSLayoutConstraint.Create (blueView, NSLayoutAttribute.Top, NSLayoutRelation.Equal, View, NSLayoutAttribute.Top, 1, 10));
-
-			var redLeftConstraint = NSLayoutConstraint.Create (redView, NSLayoutAttribute.Left, NSLayoutRelation.Equal, View, NSLayoutAttribute.Left, 1, 10);
+			var redLeftConstraint = NSLayoutConstraint.Create (redButton, NSLayoutAttribute.Left, NSLayoutRelation.Equal, View, NSLayoutAttribute.Left, 1, 10);
 			redLeftConstraint.Priority = 750;
 			View.AddConstraint (redLeftConstraint);
 
-			var marginConstraint = NSLayoutConstraint.Create (redView,NSLayoutAttribute.Right,NSLayoutRelation.Equal,View,NSLayoutAttribute.Right,1,-10);
+			var marginConstraint = NSLayoutConstraint.Create (redButton,NSLayoutAttribute.Right,NSLayoutRelation.Equal,View,NSLayoutAttribute.Right,1,-10);
 			marginConstraint.Priority = 750;
 			View.AddConstraint (marginConstraint);
 
-			var constraint = NSLayoutConstraint.Create (blueView, NSLayoutAttribute.Right, NSLayoutRelation.Equal, View, NSLayoutAttribute.Right, 1, -10);
+			var constraint = NSLayoutConstraint.Create (greenButton, NSLayoutAttribute.Right, NSLayoutRelation.Equal, View, NSLayoutAttribute.Right, 1, -10);
 			constraint.Priority = 750;
 			View.AddConstraint (constraint);
 
-			var blueLeftConstraint =NSLayoutConstraint.Create(blueView,NSLayoutAttribute.Left,NSLayoutRelation.Equal,View,NSLayoutAttribute.Left,1,10);
+			var blueLeftConstraint =NSLayoutConstraint.Create(greenButton,NSLayoutAttribute.Left,NSLayoutRelation.Equal,View,NSLayoutAttribute.Left,1,10);
 			blueLeftConstraint.Priority = 750;
 			View.AddConstraint (blueLeftConstraint);
-			redView.TouchDown += (o, s) => {
+
+			redButton.TouchDown += (o, s) => {
 				if(isBlue)
 				{
 					constraint.Priority=749;
@@ -78,7 +76,7 @@ namespace AnimatedConstraints
 				}
 			};
 
-			blueView.TouchDown+=(o,s)=>{
+			greenButton.TouchDown+=(o,s)=>{
 				if(!isRed)
 				{
 					redLeftConstraint.Priority=749;
